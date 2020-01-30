3a3733d4a2ca395b01c401ed37b4f86cff1e3a5b
De Gitgit Agitya <degitgitagitya@gmail.com>
2020-01-30 11:20:34 +0700

    first commit

---
+++ src/http.js
@@ -0,0 +1,51 @@
+class EasyHttp {
+  // Make an HTTP GET Request
+  async get(url) {
+    const response = await fetch(url);
+    const resData = await response.json();
+    return resData;
+  }
+
+  // Make an HTTP Post Request
+  async post(url, data) {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+
+    const resData = await response.json();
+    return resData;
+  }
+
+  // Make an HTTP Put Request
+  async put(url, data) {
+    const response = await fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+
+    const resData = await response.json();
+    return resData;
+  }
+
+  // Make an HTTP Delete Request
+  async delete(url) {
+    const response = await fetch(url, {
+      method: "DELETE",
+      headers: {
+        "Content-type": "application/json"
+      }
+    });
+
+    const resData = await "resource deleted";
+    return resData;
+  }
+}
+
+export const http = new EasyHttp();
